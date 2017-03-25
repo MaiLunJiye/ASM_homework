a211dc6722328b9d2cfc0c6304e20394b5cab103
simqin <mailunjiye@s.upc.edu.cn>
2017-03-25 10:19:42 +0800

    完成汇编第一到题目

--- asm/line.asm
+++ asm/line.asm
@@ -25,7 +25,48 @@ next1:
 	int 10H; 上边
   inc cx ;X坐标+1
 	cmp cx,100
+	jnz next1 ;CX不为320就继续循环DATAS SEGMENT
+	;此处输入数据段代码
+DATAS ENDS
+STACKS SEGMENT
+	;此处输入堆栈段代码
+STACKS ENDS
+CODES SEGMENT
+	ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+	MOV AX,DATAS
+	MOV DS,AX
+	mov ah,0
+	mov al,13
+	int 10h; 创建画布
+	mov ah,0bh
+	mov bh,0
+	mov bl,9
+	int 10h; 画布背景色
+	mov ah,0CH
+	mov bh,0
+	mov al,6
+
+	mov dx,20 ;把不用改变的量放到循环体外面
+next2:	
+	mov cx,20
+	inc dx
+next1:
+	int 10H; 上边
+	inc cx ;X坐标+1
+	cmp cx,100
 	jnz next1 ;CX不为320就继续循环
+	
+	cmp dx,100
+	jnz next2
+
+EXIT:
+	MOV AH,07H
+	INT 21H
+	MOV AH,4CH
+	INT 21H
+	CODES ENDS
+	END START
 next2:
 	int 10H；	
   inc DX
---
+++ verilog/multi8.v
@@ -0,0 +1,11 @@
+module multi8 (in1, in2, out);
+input reg[7:0] in1,in2;
+output reg[15:0] out;
+parameter lenth = 7;    //8 位长度
+
+always @ (in1, in2)
+begin
+
+end
+
+endmodule
